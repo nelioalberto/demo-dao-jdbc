fd30b9fefc307ce7d884f8f4cb9b6228be6bd9e2
Nelio Alberto <nekliuvekliu@gmail.com>
2023-03-07 22:30:57 -0300

    classe seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.time.LocalDateTime;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj.toString());
+		Department obj1 = new Department(1, "Books");
+		System.out.println(obj1.toString());
+		
+		LocalDateTime dtBirth = LocalDateTime.parse("1962-09-08T04:33:33");
+		
+		Seller obj2 = new Seller(1,"Nelio","nelio@gmail.com", dtBirth, 12000.0, obj1); 
+		System.out.println(obj2.toString());
 		
 	}
 
