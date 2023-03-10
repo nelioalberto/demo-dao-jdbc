af06a78002bc801fdb2c804292c3e6fd2a5795c7
Nelio Alberto <nekliuvekliu@gmail.com>
2023-03-09 22:54:45 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,17 +12,22 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
+		/* 
 		Department obj = new Department(1, "Books");
-
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
 		System.out.println(seller);
- 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller2 = sellerDao.findById(3);
+		System.out.println(seller2);
+		*/
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		Seller seller2 = sellerDao.findById(3);
+		System.out.println("=== TEST 1: seller findById =====");
+		Seller seller = sellerDao.findById(3);
 
-		System.out.println(seller2);
+		System.out.println(seller);		
+		
+		
 	}
 
 }
