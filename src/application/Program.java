485334c04685a71474425a8ef2c09b3d2da13279
Nelio Alberto <nekliuvekliu@gmail.com>
2023-03-09 23:23:11 -0300

    implementando find all

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,29 +12,36 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 
-		/* 
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(seller);
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller2 = sellerDao.findById(3);
-		System.out.println(seller2);
-		*/
+		/*
+		 * Department obj = new Department(1, "Books"); Seller seller = new Seller(21,
+		 * "Bob", "bob@gmail.com", new Date(), 3000.0, obj); System.out.println(seller);
+		 * SellerDao sellerDao = DaoFactory.createSellerDao(); Seller seller2 =
+		 * sellerDao.findById(3); System.out.println(seller2);
+		 */
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
 
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== TEST 2: seller findByDepartment =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
-		}		
-		
-		
+		}
+
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n=== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+ 
 	}
 
 }
